80299a88d0454cce20526ee91d3a4e0dbb0fa57c
Agostinho jr <agostinhojunior>
2019-11-14 21:48:15 -0300

    Tela cadastro servico completa

--- src/main/java/sc/senac/br/sistemamecanica/bean/CadastroServicoBean.java
+++ src/main/java/sc/senac/br/sistemamecanica/bean/CadastroServicoBean.java
@@ -1,23 +1,108 @@
 package sc.senac.br.sistemamecanica.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import sc.senac.br.sistemamecanica.dao.IBaseDao;
+import sc.senac.br.sistemamecanica.dao.ServicoDao;
 import sc.senac.br.sistemamecanica.model.Servico;
 
-public class CadastroServicoBean implements Serializable{
+public class CadastroServicoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	private Servico servico;
 
-	
-	
+	private Servico servico;
+	private List<Servico> servicos;
+	private List<Servico> servicosFiltro;
+	private IBaseDao<Servico> servicoDao;
+
+	@PostConstruct
+	public void init() {
+		servicoDao = new ServicoDao();
+		limpar();
+		buscar();
+	}
+
+	public void salvar() {
+		if (servico.getCodigo() == null) {
+			servicoDao.salvar(servico);
+
+			FacesMessage mensagem = new FacesMessage();
+			mensagem.setSeverity(FacesMessage.SEVERITY_INFO);
+			mensagem.setSummary("Servico salvo com sucesso!");
+
+			FacesContext.getCurrentInstance().addMessage(null, mensagem);
+
+		} else {
+			servicoDao.alterar(servico);
+
+			FacesMessage mensagem = new FacesMessage();
+			mensagem.setSeverity(FacesMessage.SEVERITY_INFO);
+			mensagem.setSummary("Servico alterado com sucesso!");
+
+			FacesContext.getCurrentInstance().addMessage(null, mensagem);
+		}
+
+		limpar();
+		buscar();
+	}
+
+	public void excluir() {
+		servicoDao.excluir(servico);
+		limpar();
+		buscar();
+
+		FacesMessage mensagem = new FacesMessage();
+		mensagem.setSeverity(FacesMessage.SEVERITY_INFO);
+		mensagem.setSummary("Servico excluído com sucesso!");
+
+		FacesContext.getCurrentInstance().addMessage(null, mensagem);
+	}
+
+	public void limpar() {
+		servico = new Servico();
+		servicos = new ArrayList<>();
+	}
+
+	public void buscar() {
+		servicos = servicoDao.buscarTodos();
+	}
+
 	public Servico getServico() {
 		return servico;
 	}
 
 	public void setServico(Servico servico) {
 		this.servico = servico;
+	}
+
+	public List<Servico> getServicos() {
+		return servicos;
+	}
+
+	public void setServicos(List<Servico> servicos) {
+		this.servicos = servicos;
+	}
+
+	public List<Servico> getServicosFiltro() {
+		return servicosFiltro;
+	}
+
+	public void setServicosFiltro(List<Servico> servicosFiltro) {
+		this.servicosFiltro = servicosFiltro;
+	}
+
+	public IBaseDao<Servico> getServicoDao() {
+		return servicoDao;
+	}
+
+	public void setServicoDao(IBaseDao<Servico> servicoDao) {
+		this.servicoDao = servicoDao;
 	}
 
 }
